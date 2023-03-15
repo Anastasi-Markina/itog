ab9f5e576a96f9be2e42ce0a4153f95b090a8d15
Markina Anastasia <aanastassias@gmail.com>
2023-03-15 14:09:11 +0300

    итог

---
+++ задача/Program.cs
@@ -0,0 +1,16 @@
+﻿            string[] array;
+            Console.WriteLine("Введите массив через пробел");
+            string stroka = Console.ReadLine();
+            array = stroka.Split(' ');
+            var result = new string[array .Length];
+            var size = 0;
+            foreach (var value in array )
+            {
+                if (value.Length <= 3)
+                {
+                    result[size] = value;
+                    size++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, size));
+            Console.ReadKey(true);
